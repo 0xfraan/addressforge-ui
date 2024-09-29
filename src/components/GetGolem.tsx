3172d2e50ce4fcd3a211f091c07cda1822c48165
Aleksander Morawski <olek.morawski@gmail.com>
2024-09-29 20:37:52 +0200

    fix hook

--- src/components/GetGolem.tsx
+++ src/components/GetGolem.tsx
@@ -6,7 +6,6 @@ export const GetGolem = () => {
   return (
     <div>
       <button
-        onClick={() => useDisconnect()}
         className="bg-blue-500 hover:bg-blue-600 text-black font-mono font-semibold py-2 px-4 rounded-md transition-colors shadow-[0_0_5px_#0000ff]"
         title="Disconnect"
       >
